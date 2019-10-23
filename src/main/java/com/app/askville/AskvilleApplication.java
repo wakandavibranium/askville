70197fc613c9d614a9778ce7d5071994be4d2b75
kamaumike <kamaumike2013@gmail.com>
2019-10-23 22:50:56 +0300

    Initial commit

---
+++ src/main/java/com/app/askville/AskvilleApplication.java
@@ -0,0 +1,13 @@
+package com.app.askville;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskvilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskvilleApplication.class, args);
+	}
+
+}

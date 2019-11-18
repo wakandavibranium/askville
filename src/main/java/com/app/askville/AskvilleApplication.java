30caceee1e5523aab0de13366121e747e6a13897
kamaumike <kamaumike2013@gmail.com>
2019-11-18 19:52:00 +0300

    Enable JPA auditing

--- src/main/java/com/app/askville/AskvilleApplication.java
+++ src/main/java/com/app/askville/AskvilleApplication.java
@@ -2,8 +2,10 @@ package com.app.askville;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AskvilleApplication {
 
 	public static void main(String[] args) {

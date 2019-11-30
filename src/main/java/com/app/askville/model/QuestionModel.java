23c05a04ecc57b0042f26e09886e3583b827acff
kamaumike <kamaumike2013@gmail.com>
2019-11-30 18:17:40 +0300

    Add Question entity
    This class is mapped to the 'questions' table in the database

---
+++ src/main/java/com/app/askville/model/QuestionModel.java
@@ -0,0 +1,54 @@
+package com.app.askville.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "questions")
+public class QuestionModel extends AuditModel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Size(min = 3, max = 100)
+	private String title;
+	
+	@NotNull
+	@Column(name = "description")
+	private String description;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+
+}

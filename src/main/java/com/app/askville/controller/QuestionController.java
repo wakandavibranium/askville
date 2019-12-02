d8a459bc949a67b16cfc7f662fac15c87dd25bfc
kamaumike <kamaumike2013@gmail.com>
2019-12-02 22:24:38 +0300

    Add function for getting a single question by id

--- src/main/java/com/app/askville/controller/QuestionController.java
+++ src/main/java/com/app/askville/controller/QuestionController.java
@@ -1,8 +1,14 @@
 package com.app.askville.controller;
 
+
+
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +27,12 @@ public class QuestionController {
 	@PostMapping("/questions")
 	public QuestionModel createQuestion(@Valid @RequestBody QuestionModel question) {
 		return questionRepository.save(question);
+	}
+	
+	//Fetch specific question by id
+	@GetMapping("/questions/{id}")
+	public Optional<QuestionModel> getQuestionById(@PathVariable Long id) {
+		return questionRepository.findById(id);
 	}
 
 }
